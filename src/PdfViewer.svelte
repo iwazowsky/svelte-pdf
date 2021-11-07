3797753258388430115904998375ae2e7245a7bd
Vazgen Harutyunyan <hvazgen@gmail.com>
2021-11-08 00:21:41 +0400

    brr

--- src/PdfViewer.svelte
+++ src/PdfViewer.svelte
@@ -2,7 +2,7 @@
   import { onDestroy, tick } from "svelte";
   import pdfjs from "pdfjs-dist";
   import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
-  import {FileSaver} from "file-saver";
+  import FileSaver from "file-saver";
   import { onPrint, calcRT, getPageText } from "./utils/Helper.svelte";
   import Tooltip from "./utils/Tooltip.svelte";
 
